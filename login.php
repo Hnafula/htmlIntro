5c14f44db9d61e1882200abadf083849e3d1b5bb
Hnafula <hildasichangi@gmail.com>
2017-10-26 20:22:23 +0300

    html

---
+++ login.php
@@ -0,0 +1,21 @@
+<html>
+
+<form name = "form1" method = "post" width = "100%" height="100%">
+
+ 
+ 
+ <label> Name:</label style = "align = center";>
+ <input type = "text" name="name1" >
+ <br /><br />
+<label>Password</label style = "align = center";>
+<input type = "password" name="pass1">
+<br /><br />
+
+<input type="submit" value="submit">
+
+<input type = "reset" value ="reset">
+
+
+
+</form>
+</html>
